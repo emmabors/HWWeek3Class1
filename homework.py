b053b905c6b2f3ad8473da29a9f597a527ba4189
Emma Borsavage <emma_borsavage@aol.com>
2023-02-19 14:31:53 -0700

    changed

--- homework.py
+++ homework.py
@@ -1,7 +1,6 @@
 import re
 with open('regex_test (1).txt') as my_file:
     data = my_file.readlines()
-    print(data)
     for name in data:
         pattern = re.compile(r'[A-Za-z]+[" "][A-Z][a-zA-Z]+')
         match = pattern.findall(name)
