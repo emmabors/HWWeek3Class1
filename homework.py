0979a22aaf1ecb7dabe42abaaa1fcc3812f76107
Emma Borsavage <emma_borsavage@aol.com>
2023-02-19 14:30:55 -0700

    first commit

---
+++ homework.py
@@ -0,0 +1,14 @@
+import re
+with open('regex_test (1).txt') as my_file:
+    data = my_file.readlines()
+    print(data)
+    for name in data:
+        pattern = re.compile(r'[A-Za-z]+[" "][A-Z][a-zA-Z]+')
+        match = pattern.findall(name)
+        if match:
+            print(name)
+        else:
+            print(None)
+
+
+
